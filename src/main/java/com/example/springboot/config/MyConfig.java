bd0c3e389082eebf23464f33559cdf20effe5357
chengmengd <chengmengd@isoftstone.com>
2026-09-01 02:09:27 +0800

    @Import注解详解

--- src/main/java/com/example/springboot/config/MyConfig.java
+++ src/main/java/com/example/springboot/config/MyConfig.java
@@ -4,6 +4,7 @@ import com.example.springboot.bean.Pet;
 import com.example.springboot.bean.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * 1、配置类里面使用@Bean标注在方法上给容器注册组件，默认也是单实例的
@@ -21,7 +22,10 @@ import org.springframework.context.annotation.Configuration;
  *          2、减少代理开销，提升启动速度，降低内存占用，适合性能敏感场景。
  *          3、方法间直接调用会创建新实例，破坏单例特性；无法通过配置类方法调用处理循环依赖。
  *          4、配置类组件之间无依赖关系，用Lite模式加速容器启动过程，减少判断。
+ *
+ * @Import注解的作用是给容器中自动创建出指定的组件，默认组件名称是全类名。
  */
+@Import({User.class})
 @Configuration(proxyBeanMethods = true)
 public class MyConfig {
 
